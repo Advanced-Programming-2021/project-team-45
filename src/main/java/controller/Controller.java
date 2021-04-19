e06f15878d294730fb77a1b1afee833ba2e800f2
Kasra <kasrahajian2002@gmail.com>
2021-04-19 18:58:58 +0430

    start of controller classes and add Login and Profile controllers

--- src/main/java/controller/Controller.java
+++ src/main/java/controller/Controller.java
@@ -1,4 +1,16 @@
 package controller;
 
-public class Controller {
+import model.user.User;
+
+public abstract class Controller {
+
+    protected final User user;
+    protected final String username;
+
+
+    protected Controller(String username) {
+        this.user = User.getUserByUsername(username);
+        this.username = username;
+    }
+
 }
