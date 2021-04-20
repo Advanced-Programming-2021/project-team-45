122ed6154438ba1cf081012b657621be7b872b60
hm3623851 <79132538+hm3623851@users.noreply.github.com>
2021-04-20 12:51:23 +0430

    abilities Of Monster

--- src/main/java/model/card/MonsterCard.java
+++ src/main/java/model/card/MonsterCard.java
@@ -6,6 +6,7 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Iterator;
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
