c93e1e8c82a92da6103ef8124780a1aa46827a88
hm3623851 <79132538+hm3623851@users.noreply.github.com>
2021-04-20 13:29:59 +0430

    spell Abilities

--- src/main/java/model/card/AbilitiesOfSpellAndTrap.java
+++ src/main/java/model/card/AbilitiesOfSpellAndTrap.java
@@ -1,5 +1,180 @@
 package model.card;
 
 public class AbilitiesOfSpellAndTrap {
+/*
+    spell Names
+        Monster Reborn
+        Terraforming
+        Pot of Greed
+        Raigeki
+        Change of Heart
+        Harpie’s Feather Duster
+        Swords of Revealing Light
+        Dark Hole
+        Supply Squad
+        Spell Absorption
+        Messenger of peace
+        Twin Twisters
+        Mystical space typhoon
+        Ring of Defense
+        Yami
+        Forest
+        Closed Forest
+        UMIIRUKA
+        Sword of Dark Destruction
+        Black Pendant
+        United We Stand
+        Magnum Shield
+        Advanced Ritual Art
 
+    trap names
+        Magic Cylinder
+        Mirror Force
+        Mind Crush
+        Trap Hole
+        Torrential Tribute
+        Time Seal
+        Negate Attack
+        Solemn Warning
+        Magic Jammer
+        Call of the Haunted
+
+
+*/
+    public void spellAndTrapAbilities(SpellTrapCard card){
+        switch (card.cardName){
+            case " Monster Reborn":
+                reborn();
+                break;
+            case "Terraforming":
+                Terraforming();
+                break;
+            case "Pot of Greed":
+                potGreed();
+                break;
+            case "Raigeki":
+                Raigeki();
+                break;
+            case "Change of Heart":
+                changeHeart();
+                break;
+            case "Harpie’s Feather Duster":
+                featherDuster();
+                break;
+            case "Swords of Revealing Light":
+                swordLight();
+                break;
+            case "Dark Hole":
+                darkHole();
+                break;
+            case "Supply Squad":
+                supplySquad();
+                break;
+            case "Spell Absorption":
+                absorption();
+                break;
+            case "Messenger of peace":
+                messenger();
+                break;
+            case "Twin Twisters":
+                twin();
+                break;
+            case "Mystical space typhoon":
+                typhoon();
+                break;
+            case "Ring of Defense":
+                ringOfDefense();
+                break;
+            case "Yami":
+                yami();
+                break;
+            case "Forest":
+                forest();
+                break;
+            case "Closed Forest":
+                closedForest();
+                break;
+            case "UMIIRUKA":
+                umiiruka();
+                break;
+            case "Sword of Dark Destruction":
+                sword();
+                break;
+            case "Black Pendant":
+                pendant();
+                break;
+            case "United We Stand":
+                unitedStand();
+                break;
+            case "Magnum Shield":
+                magnum();
+                break;
+            case "Advanced Ritual Art":
+                ritualArt();
+                break;
+            case "Magic Cylinder":
+                magic();
+                break;
+            case "Mirror Force":
+                mirror();
+                break;
+            case "Mind Crush":
+                crush();
+                break;
+            case "Trap Hole":
+                trapHole();
+                break;
+            case "Torrential Tribute":
+                tribute();
+                break;
+            case "Time Seal":
+                seal();
+                break;
+            case "Negate Attack":
+                neggateAttack();
+                break;
+            case "Solemn Warning":
+                solemWarning();
+                break;
+            case "Magic Jammer":
+                MagicJammer();
+                break;
+            case "Call of the Haunted":
+                call();
+                break;
+        }
+    }
+    private void reborn(){}
+    private void Terraforming(){}
+    private void potGreed(){}
+    private void Raigeki(){}
+    private void changeHeart(){}
+    private void featherDuster(){}
+    private void swordLight(){}
+    private void darkHole(){}
+    private void supplySquad(){}
+    private void absorption(){}
+    private void messenger(){}
+    private void twin(){}
+    private void typhoon(){}
+    private void ringOfDefense(){}
+    private void yami(){}
+    private void forest(){}
+    private void closedForest(){}
+    private void umiiruka(){}
+    private void sword(){}
+    private void pendant(){}
+    private void unitedStand(){}
+    private void magnum(){}
+    private void ritualArt(){}
+    private void magic(){}
+    private void mirror(){}
+    private void crush(){}
+    private void trapHole(){}
+    private void tribute(){}
+    private void seal(){}
+    private void neggateAttack(){}
+    private void solemWarning(){}
+    private void MagicJammer(){}
+    private void call(){}
 }
