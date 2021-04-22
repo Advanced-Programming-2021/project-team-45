73917a9c7a4a8000402cf269c499fee3c35beb2f
amir-haji <haji80as@gmail.com>
2021-04-22 13:21:27 +0430

    graveyard and hand started

--- src/main/java/model/Game/Graveyard.java
+++ src/main/java/model/Game/Graveyard.java
@@ -1,4 +1,43 @@
 package model.Game;
 
+import model.card.Card;
+
+import java.util.ArrayList;
+
 public class Graveyard {
+    private ArrayList<Card> graveyardCards;
+
+    public void addCardToGraveyard(Card card){
+        (this.graveyardCards).add(card);
+    }
+
+    public void deleteCardFromGraveyard(Card card){
+        (this.graveyardCards).remove(card);
+    }
+
+    public boolean doesCardExistInGraveyard(Card card){
+        return (this.graveyardCards).contains(card);
+    }
+
+    public Card getCardFromGraveyard(String cardName){
+        ArrayList<Card> targetCard = new ArrayList<>();
+        for(Card card : this.graveyardCards){
+            if((card.getCardName()).equals(cardName))
+                targetCard.add(card);
+        }
+        if(targetCard.size() == 0) return null; else return targetCard.get(0);
+    }
+
+    public ArrayList<String> getGraveyardStr(){
+        ArrayList<String> graveyardStr = new ArrayList<>();
+        if(this.graveyardCards.size() == 0)
+            graveyardStr.add("graveyard empty");
+        else{
+            for(int i = 0; i < (this.graveyardCards).size(); i++){
+                Card card = (this.graveyardCards).get(i);
+                graveyardStr.add((i + 1) + ". " + card.getCardName() + ":" + card.getCardDescription());
+            }
+        }
+         return graveyardStr;
+    }
 }
