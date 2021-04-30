a7f481b463c4a00cb2a5094489406a4e74c9b423
Kasra <kasrahajian2002@gmail.com>
2021-04-30 23:00:58 +0430

    finishe mainMenuController and minor fixes in UserDeck

--- src/main/java/controller/MainMenuController.java
+++ src/main/java/controller/MainMenuController.java
@@ -1,5 +1,7 @@
 package controller;
 
+import model.user.User;
+
 public class MainMenuController extends Controller {
 
     public MainMenuController(String username) {
@@ -7,6 +9,28 @@ public class MainMenuController extends Controller {
     }
 
     public int startGameErrorHandler(String opponentUsername, int rounds) {
+        User opponent = User.getUserByUsername(opponentUsername);
+
+        if (!User.doesUsernameExist(opponentUsername)) {
+            return 1;
+
+        } else if (!user.getUserDeck().doesExistActiveDeck()) {
+            return 2;
+
+        } else if (!opponent.getUserDeck().doesExistActiveDeck()) {
+            return 3;
+
+        } else if (!user.getUserDeck().isActiveDeckValid()) {
+            return 4;
+
+        } else if (!opponent.getUserDeck().isActiveDeckValid()) {
+            return 5;
+
+        } else if (!(rounds == 1 || rounds == 3)) {
+            return 6;
+
+        }
+
         return 0;
     }
 
