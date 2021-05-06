64ed9c0e108b5a8df0d32be2a388e299dde399e8
Kasra <kasrahajian2002@gmail.com>
2021-05-06 21:37:38 +0430

    fixed ImportExport

--- src/main/java/controller/DatabaseController.java
+++ src/main/java/controller/DatabaseController.java
@@ -6,8 +6,11 @@ import model.card.Card;
 import model.card.MonsterCard;
 import model.card.SpellTrapCard;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Scanner;
 
 public class DatabaseController extends Controller {
 
@@ -18,7 +21,7 @@ public class DatabaseController extends Controller {
 
     private void writeToExportedCards(String json) {
         try {
-            FileWriter writer = new FileWriter("..\\..\\resources\\ImportedCards.txt");
+            FileWriter writer = new FileWriter("database\\exported.txt");
             writer.write(json);
             writer.close();
 
@@ -29,24 +32,36 @@ public class DatabaseController extends Controller {
     }
 
     public void importCard(String cardName) {
-
+        File file = new File("database\\exported.txt");
+        Scanner fileScanner = null;
+        try {
+            fileScanner = new Scanner(file);
+        } catch (FileNotFoundException e) {
+            System.out.println("can not read file!");
+        }
+        String cardType = fileScanner.nextLine();
+        String json = fileScanner.nextLine();
+        Card card;
+        if (cardType.equals("monster")) {
+            card = (new Gson()).fromJson(json, MonsterCard.class);
+        } else {
+            card = (new Gson()).fromJson(json, SpellTrapCard.class);
+        }
+        user.getCardInventory().addCardToInventory(card);
     }
 
     public void exportCard(String cardName) {
-        Gson gson = new GsonBuilder().setPrettyPrinting().create();
-
         Card card = user.getCardInventory().getCardByCardName(cardName);
         String jsonString;
 
         if (card instanceof MonsterCard) {
             MonsterCard monsterCard = (MonsterCard) card;
-            jsonString = gson.toJson(monsterCard);
+            jsonString = "monster\n" + (new Gson()).toJson(monsterCard);
         } else {
             SpellTrapCard spellTrapCard = (SpellTrapCard) card;
-            jsonString = gson.toJson(spellTrapCard);
+            jsonString = "spell\n" + (new Gson()).toJson(spellTrapCard);
         }
 
         writeToExportedCards(jsonString);
     }
-
 }
