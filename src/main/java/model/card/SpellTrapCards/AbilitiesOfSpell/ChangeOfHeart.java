856b84a56701bf75448b14b85d480cc346a640dc
amir-haji <haji80as@gmail.com>
2021-05-06 09:41:29 +0430

    change of hurt added

--- src/main/java/model/card/SpellTrapCards/AbilitiesOfSpell/ChangeOfHeart.java
+++ src/main/java/model/card/SpellTrapCards/AbilitiesOfSpell/ChangeOfHeart.java
@@ -1,8 +1,37 @@
 package model.card.SpellTrapCards.AbilitiesOfSpell;
 
-public class ChangeOfHeart {
-    public static void ability(){
+import model.Game.Game;
+import model.Game.GameBoard;
+import model.card.MonsterCard;
 
+public class ChangeOfHeart {
+
+    private static int cardPosition = 0;
+    private static MonsterCard targetMonsterCard;
+
+    public static void ability(Game game){
+        // input one card from opponent  monster field
+        GameBoard playerGameBoard  = game.getGameBoardOfPlayerOfThisTurn();
+        GameBoard opponentGameBoard = game.getGameBoardOfOpponentPlayerOfThisTurn();
+        MonsterCard monsterCard;
+        targetMonsterCard = monsterCard;
+        for(int i = 0; i < 5; i++) {
+            if(opponentGameBoard.getMonsterField().getMonstersOnField()[i].equals(monsterCard)) {
+                cardPosition = i;
+                break;
+            }
+        }
+        if(!playerGameBoard.getMonsterField().isFull()) {
+            playerGameBoard.getMonsterField().addMonsterToField(monsterCard);
+            opponentGameBoard.getMonsterField().deleteMonster(monsterCard);
+        }
     }
 
+    public static void deActiveAbility(Game game) {
+        GameBoard playerGameBoard  = game.getGameBoardOfPlayerOfThisTurn();
+        GameBoard opponentGameBoard = game.getGameBoardOfOpponentPlayerOfThisTurn();
+
+        opponentGameBoard.getMonsterField().addMonsterToField(targetMonsterCard);
+        playerGameBoard.getMonsterField().deleteMonster(targetMonsterCard);
+    }
 }
