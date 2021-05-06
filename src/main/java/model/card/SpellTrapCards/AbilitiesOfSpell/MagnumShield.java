1330d9b01f0e1b3b955a2eeb738466532860c1ad
amir-haji <haji80as@gmail.com>
2021-05-06 10:45:31 +0430

    magnum shield added

--- src/main/java/model/card/SpellTrapCards/AbilitiesOfSpell/MagnumShield.java
+++ src/main/java/model/card/SpellTrapCards/AbilitiesOfSpell/MagnumShield.java
@@ -1,8 +1,17 @@
 package model.card.SpellTrapCards.AbilitiesOfSpell;
 
+import model.Game.Game;
+import model.card.MonsterCard;
+import model.card.PositionMonsters;
+
 public class MagnumShield {
 
-    public static void ability(){
-
+    public static void ability(Game game){
+        // input one card of Type Warrior from field
+        MonsterCard monsterCard;
+        if(monsterCard.getPosition() == PositionMonsters.ATTACK)
+            monsterCard.increaseAttack(monsterCard.getDefense());
+        else
+            monsterCard.increaseDefense(monsterCard.getAttack());
     }
 }
