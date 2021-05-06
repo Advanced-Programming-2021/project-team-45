6df773deb10742ecfe776dc6c01c7e0abe5e3ca8
amir-haji <haji80as@gmail.com>
2021-05-07 00:26:33 +0430

    graveyardTest just added

---
+++ src/test/java/model/Game/GraveyardTest.java
@@ -0,0 +1,43 @@
+package model.Game;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GraveyardTest {
+    @BeforeEach
+    public void set() {
+
+    }
+
+    @Test
+    void addCardToGraveYard() {
+
+    }
+
+    @Test
+    void deleteCardFromGraveyard() {
+
+    }
+
+    @Test
+    void doesCardExistInGraveyard() {
+
+    }
+
+    @Test
+    void getCardFromGraveyard() {
+
+    }
+
+    @Test
+    void getGraveyardStr() {
+
+    }
+
+
+
+
+
+}
