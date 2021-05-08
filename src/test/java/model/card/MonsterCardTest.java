9429c8a652d9a725daea87859779298ba942a568
amir-haji <haji80as@gmail.com>
2021-05-08 22:26:59 +0430

    MonsterCard Test changed

--- src/test/java/model/card/MonsterCardTest.java
+++ src/test/java/model/card/MonsterCardTest.java
@@ -15,32 +15,50 @@ class MonsterCardTest {
 
     @Test
     void increaseAttack() {
-
+        MonsterCard monsterCard = (MonsterCard) Card.getCardByName("Battle OX");
+        monsterCard.increaseAttack(200);
+        assertEquals(1900, monsterCard.getAttack());
+        monsterCard.decreaseAttack(200);
     }
 
     @Test
     void decreaseAttack() {
-
+        MonsterCard monsterCard = (MonsterCard) Card.getCardByName("Horn Imp");
+        monsterCard.decreaseAttack(200);
+        assertEquals(1100, monsterCard.getAttack());
+        monsterCard.increaseAttack(200);
     }
 
     @Test
     void increaseDefense() {
-
+        MonsterCard monsterCard = (MonsterCard) Card.getCardByName("Fireyarou");
+        monsterCard.increaseDefense(200);
+        assertEquals(1200, monsterCard.getDefense());
+        monsterCard.decreaseAttack(200);
     }
 
     @Test
     void decreaseDefense() {
-
+        MonsterCard monsterCard = (MonsterCard) Card.getCardByName("Suijin");
+        monsterCard.decreaseDefense(200);
+        assertEquals(2200, monsterCard.getDefense());
+        monsterCard.increaseDefense(200);
     }
 
     @Test
     void setAttack() {
-
+        MonsterCard monsterCard = (MonsterCard) Card.getCardByName("Haniwa");
+        monsterCard.setAttack(600);
+        assertEquals(600, monsterCard.getAttack());
+        monsterCard.setAttack(500);
     }
 
     @Test
     void setDefense() {
-
+        MonsterCard monsterCard = (MonsterCard) Card.getCardByName("Axe Raider");
+        monsterCard.setDefense(1000);
+        assertEquals(1000, monsterCard.getDefense());
+        monsterCard.setDefense(1150);
     }
 
     @Test
