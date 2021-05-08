18162401b608bb710496ac007c712f3de3d4849c
amir-haji <haji80as@gmail.com>
2021-05-08 21:27:20 +0430

    cardTest changed

--- src/test/java/model/card/CardTest.java
+++ src/test/java/model/card/CardTest.java
@@ -15,22 +15,29 @@ class CardTest {
 
     @Test
     void getCardByName() {
-
+        assertNull(Card.getCardByName("hajji"));
+        assertNotNull(Card.getCardByName("Yomi Ship"));
+        assertEquals("Yomi Ship", Card.getCardByName("Yomi Ship").getCardName());
     }
 
     @Test
     void getCardName() {
-
+        assertEquals("Axe Raider", Card.getCardByName("Axe Raider"));
+        assertNotEquals("Yomi Ship", Card.getCardByName("Horn Imp").getCardName());
     }
 
     @Test
     void getCardDescription() {
-
+        assertEquals("A small fiend that dwells in the dark, its single horn makes it a formidable opponent.\n",
+                Card.getCardByName("Horn Imp").getCardDescription());
+        assertNotEquals("A small fiend that dwells in the dark, its single horn makes it a formidable opponent.\n",
+                Card.getCardByName("Axe Raider").getCardDescription());
     }
 
     @Test
     void getPrice() {
-
+        assertEquals(3000, Card.getCardByName("Solemn Warning").getPrice());
+        assertNotEquals(1300, Card.getCardByName("Silver Fang").getPrice());
     }
 
     @Test
@@ -40,7 +47,9 @@ class CardTest {
 
     @Test
     void showCard() {
-
+        assertEquals("Name: Horn Imp\n" + "Level: 4\n" + "Type: Normal\n" + "ATK: 1300\n" + "DEF: 1000\n" +
+                "Description: A small fiend that dwells in the dark, its single horn makes it a formidable opponent.\n",
+                Card.getCardByName("Horn Imp").getCardDescription());
     }
 
     @AfterAll
