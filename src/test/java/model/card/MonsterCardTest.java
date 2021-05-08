b400e48b4d20cebb6fd5119cc13cc333975fb0a1
amir-haji <haji80as@gmail.com>
2021-05-08 21:51:32 +0430

    monsterCardTest added

---
+++ src/test/java/model/card/MonsterCardTest.java
@@ -0,0 +1,124 @@
+package model.card;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MonsterCardTest {
+
+    @BeforeAll
+    public void setBeforeTest() {
+
+    }
+
+    @Test
+    void increaseAttack() {
+
+    }
+
+    @Test
+    void decreaseAttack() {
+
+    }
+
+    @Test
+    void increaseDefense() {
+
+    }
+
+    @Test
+    void decreaseDefense() {
+
+    }
+
+    @Test
+    void setAttack() {
+
+    }
+
+    @Test
+    void setDefense() {
+
+    }
+
+    @Test
+    void changePosition() {
+
+    }
+
+    @Test
+    void summon() {
+
+    }
+
+    @Test
+    void set() {
+
+    }
+
+    @Test
+    void attackMonster() {
+
+    }
+
+    @Test
+    void attackOpponent() {
+
+    }
+
+    @Test
+    void getAttack() {
+
+    }
+
+    @Test
+    void getDefense() {
+
+    }
+
+    @Test
+    void getSpecial() {
+
+    }
+
+    @Test
+    void getLevel() {
+
+    }
+
+    @Test
+    void getPosition() {
+
+    }
+
+    @Test
+    void getDefenceMode() {
+
+    }
+
+    @Test
+    void setDefenceMode() {
+
+    }
+
+    @Test
+    void getAttribute() {
+
+    }
+
+    @Test
+    void getType() {
+
+    }
+
+    void getSpecialMonsterEnum() {
+
+    }
+
+    @AfterAll
+    public void setAfterTest() {
+
+    }
+}
