27bcec2fedfc3fc304392e7c88e9aae3b05c877a
amir-haji <haji80as@gmail.com>
2021-05-08 19:53:18 +0430

    shop test changed

--- src/test/java/model/ShopTest.java
+++ src/test/java/model/ShopTest.java
@@ -1,15 +1,18 @@
 package model;
 
 import model.user.User;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class ShopTest {
-    @BeforeEach
+    @BeforeAll
     public void set() {
         User user = new User("hajji", "hajji", "hajji");
+        user.increaseMoney(1800);
     }
 
     @Test
@@ -19,7 +22,10 @@ class ShopTest {
 
     @Test
     void hasEnoughMoney() {
-
+        Shop shop = new Shop("hajji");
+        User user = User.getUserByUsername("hajji");
+        assertTrue(shop.hasEnoughMoney("Yomi Ship"));
+        assertFalse(shop.hasEnoughMoney("Horn Imp"));
     }
 
     @Test
@@ -28,5 +34,10 @@ class ShopTest {
         shop.buy("Yomi Ship");
         assertNotNull(User.getUserByUsername("hajji").getCardInventory().getCardByCardName("Yomi Ship"));
         assertNull(User.getUserByUsername("hajji").getCardInventory().getCardByCardName("Battle OX"));
+    }
+
+    @AfterAll
+    public void setAfterTest() {
+        User.deleteUserByUsername("hajji");
     }
 }
