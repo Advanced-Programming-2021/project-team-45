130b845a7e05d5bcb79951a980239112a5640589
amir-haji <haji80as@gmail.com>
2021-05-08 19:45:21 +0430

    deckTest just added

---
+++ src/test/java/model/card/DeckTest.java
@@ -0,0 +1,74 @@
+package model.card;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DeckTest {
+    @BeforeAll
+    public void set() {
+
+    }
+
+    @Test
+    void addCard() {
+
+    }
+
+    @Test
+    void deleteCard() {
+
+    }
+
+    @Test
+    void doesCardExist() {
+
+    }
+
+    @Test
+    void getCardCountInDeck() {
+
+    }
+
+    @Test
+    void isFullFromCard() {
+
+    }
+
+    @Test
+    void isFull() {
+
+    }
+
+    @Test
+    void isDeckValid() {
+
+    }
+
+    @Test
+    void getName() {
+
+    }
+
+    @Test
+    void getMainDeck() {
+
+    }
+
+    @Test
+    void getSideDeck() {
+
+    }
+
+    @Test
+    void getMonsterStr() {
+
+    }
+
+    @Test
+    void getSpellsAndTrapStr() {
+
+    }
+
+}
