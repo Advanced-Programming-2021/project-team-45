7009ae886f9a8a4ee2dff86ea449248397a9a609
amir-haji <haji80as@gmail.com>
2021-05-08 20:58:51 +0430

    cardTest added

---
+++ src/test/java/model/card/CardTest.java
@@ -0,0 +1,50 @@
+package model.card;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CardTest {
+
+    @BeforeAll
+    public void set() {
+
+    }
+
+    @Test
+    void getCardByName() {
+
+    }
+
+    @Test
+    void getCardName() {
+
+    }
+
+    @Test
+    void getCardDescription() {
+
+    }
+
+    @Test
+    void getPrice() {
+
+    }
+
+    @Test
+    void getOwner() {
+
+    }
+
+    @Test
+    void showCard() {
+
+    }
+
+    @AfterAll
+    public void setAfterTest() {
+
+    }
+}
