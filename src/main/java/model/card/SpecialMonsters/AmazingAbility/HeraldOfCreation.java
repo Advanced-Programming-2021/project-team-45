7fec754f96c03e60c921a9a4fe8eec1edaccf05a
hm3623851 <79132538+hm3623851@users.noreply.github.com>
2021-05-09 04:45:13 +0430

    monsters

--- src/main/java/model/card/SpecialMonsters/AmazingAbility/HeraldOfCreation.java
+++ src/main/java/model/card/SpecialMonsters/AmazingAbility/HeraldOfCreation.java
@@ -2,6 +2,7 @@ package model.card.SpecialMonsters.AmazingAbility;
 
 import model.game.Game;
 import model.game.GameBoard;
+import model.game.fields.CardField;
 import model.game.fields.MonsterField;
 import model.card.Card;
 import model.card.MonsterCard;
@@ -16,12 +17,39 @@ public class HeraldOfCreation {
         if(!heraldCards.contains(thisHerald)) {
             heraldCards.add(thisHerald);
             // one input card from hand
-            Card card;
-            GameBoard playerGameBoard = game.getGameBoardOfPlayerOfThisTurn();
-            playerGameBoard.getHand().deleteCard(card);
-            // input one monster Card with minimum level of 7 from player graveyard
-            MonsterCard monsterCard;
-            playerGameBoard.getHand().addCard(monsterCard);
+            CardField[] cardFields={game.getGameBoardOfPlayerOfThisTurn().getHand()};
+            ArrayList<Card> card=game.getGameController().getCardFromPlayer(1,cardFields);
+            if(card.get(0)!=null) {
+                GameBoard playerGameBoard = game.getGameBoardOfPlayerOfThisTurn();
+                playerGameBoard.getHand().deleteCard(card.get(0));
+                // input one monster Card with minimum level of 7 from player graveyard
+                MonsterCard monsterCard=getMonster(game,card.get(0));
+                if(monsterCard!=null) {
+                    playerGameBoard.getHand().addCard(monsterCard);
+                }
+            }
+        }
+    }
+    
+    private static MonsterCard getMonster(Game game ,Card DeletedCard){
+        CardField[] cardFields={game.getGameBoardOfPlayerOfThisTurn().getGraveyard()};
+        ArrayList<Card>  card=game.getGameController().getCardFromPlayer(1,cardFields);
+        if(card.get(0)!=null){
+            if(card.get(0) instanceof MonsterCard){
+                if(((MonsterCard) card.get(0)).getLevel()>=7){
+                    return (MonsterCard) card.get(0);
+                }else{
+                    game.getGameController().showOutput("your choice is wrong");
+                    game.getGameBoardOfPlayerOfThisTurn().getHand().addCard(DeletedCard);
+                    return null;
+                }
+            }else{
+                game.getGameController().showOutput("your choice is wrong");
+                game.getGameBoardOfPlayerOfThisTurn().getHand().addCard(DeletedCard);
+                return null;
+            }
+        }else{
+            return null;
         }
     }
 
