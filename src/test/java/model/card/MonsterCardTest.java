2417b88801a6209a756ccac5516746d250456659
amir-haji <haji80as@gmail.com>
2021-05-09 20:37:03 +0430

    monsterCard test changed

--- src/test/java/model/card/MonsterCardTest.java
+++ src/test/java/model/card/MonsterCardTest.java
@@ -1,8 +1,10 @@
 package model.card;
 
+import model.user.User;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -10,7 +12,7 @@ class MonsterCardTest {
 
     @BeforeAll
     public void setBeforeTest() {
-
+        User user = new User("hajji", "hajji", "hajji");
     }
 
     @Test
@@ -88,22 +90,26 @@ class MonsterCardTest {
 
     @Test
     void getAttack() {
-
+        MonsterCard monsterCard = (MonsterCard) Card.getCardByName("Silver Fang");
+        assertEquals(1200, monsterCard.getAttack());
     }
 
     @Test
     void getDefense() {
-
+        MonsterCard monsterCard = (MonsterCard) Card.getCardByName("Crab Turtle");
+        assertEquals(2500, monsterCard.getDefense());
     }
 
     @Test
     void getSpecial() {
-
+        MonsterCard monsterCard1 = (MonsterCard) Card.getCardByName("Axe Raider");
+        assertNull(monsterCard1.getSpecial());
     }
 
     @Test
     void getLevel() {
-
+        MonsterCard monsterCard = (MonsterCard) Card.getCardByName("Wattkid");
+        assertEquals(3, monsterCard.getLevel());
     }
 
     @Test
@@ -123,20 +129,29 @@ class MonsterCardTest {
 
     @Test
     void getAttribute() {
-
+        MonsterCard monsterCard1 = (MonsterCard) Card.getCardByName("Fireyarou");
+        assertEquals(MonsterAttribute.FIRE, monsterCard1.getAttribute());
+        MonsterCard monsterCard2 = (MonsterCard) Card.getCardByName("Baby Dragon");
+        assertEquals(MonsterAttribute.WIND, monsterCard2.getAttribute());
     }
 
     @Test
     void getType() {
-
+        MonsterCard monsterCard1 = (MonsterCard) Card.getCardByName("Battle OX");
+        assertEquals(MonsterType.Beast_Warrior, monsterCard1.getType());
+        MonsterCard monsterCard2 = (MonsterCard) Card.getCardByName("Horn Imp");
+        assertEquals(MonsterType.Fiend, monsterCard2.getType());
     }
 
     void getSpecialMonsterEnum() {
-
+        MonsterCard monsterCard1 = (MonsterCard) Card.getCardByName("Horn Imp");
+        assertNull(monsterCard1.getSpecialMonsterEnum());
+        MonsterCard monsterCard2 = (MonsterCard) Card.getCardByName("Suijin");
+        assertEquals(SpecialMonsterEnum.SUIJIN, monsterCard2.getSpecialMonsterEnum());
     }
 
     @AfterAll
     public void setAfterTest() {
-
+        User.deleteUserByUsername("hajji");
     }
 }
