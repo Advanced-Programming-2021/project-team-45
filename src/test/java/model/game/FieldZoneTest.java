fbc586887599db1fe89e49423e0be0333fc7cb17
amir-haji <haji80as@gmail.com>
2021-05-10 00:15:12 +0430

    fieldZone test completed

--- src/test/java/model/game/FieldZoneTest.java
+++ src/test/java/model/game/FieldZoneTest.java
@@ -1,37 +1,51 @@
 package model.game;
 
+import model.card.Card;
+import model.game.fields.FieldZone;
+import model.game.fields.Graveyard;
 import org.junit.jupiter.api.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 class FieldZoneTest {
+    private Graveyard graveyard;
+    private FieldZone fieldZone;
     @BeforeAll
     public void set() {
-
-    }
-
-    @Test
-    void doesCardExist() {
-
-    }
-
-    @Test
-    void getFieldCard() {
-
-    }
-
-    @Test
-    void setFieldCard() {
-
+        this.graveyard = new Graveyard();
+        this.fieldZone = new FieldZone(this.graveyard);
     }
 
     @Test
     void isFull() {
+        assertFalse(this.fieldZone.isFull());
+        this.fieldZone.setFieldCard(Card.getCardByName("Forest"));
+        assertTrue(this.fieldZone.isFull());
+    }
+
+    @Test
+    void doesCardExist() {
+        assertTrue(this.fieldZone.doesCardExist("Forest"));
+        assertFalse(this.fieldZone.doesCardExist("Closed Forest"));
+    }
+
+    @Test
+    void getFieldCard() {
+        assertSame(Card.getCardByName("Forest"), this.fieldZone.getFieldCard());
+    }
+
+    @Test
+    void setFieldCard() {
+        this.fieldZone.setFieldCard(Card.getCardByName("Closed Forest"));
+        assertTrue(this.fieldZone.doesCardExist("Closed Forest"));
+        assertTrue(this.graveyard.doesCardExist("Forest"));
 
     }
 
+
     @AfterAll
     public void setAfterTest() {
-
+        this.fieldZone = null;
+        this.graveyard = null;
     }
 
 }
