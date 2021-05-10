2f03c2e944badebd9a867741d2433070fb51ea4d
amir-haji <haji80as@gmail.com>
2021-05-10 23:24:21 +0430

    spellTrap field test added

---
+++ src/test/java/model/game/fields/SpellTrapFieldTest.java
@@ -0,0 +1,70 @@
+package model.game.fields;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SpellTrapFieldTest {
+
+    @BeforeAll
+    public void setBeforeTest() {
+
+    }
+
+    @Test
+    void getSpellTrapCardsPositionsArray() {
+
+    }
+
+    @Test
+    void getPlayerSpellTrapCard() {
+
+    }
+
+    @Test
+    void getOpponentSpellTrapCard() {
+
+    }
+
+    @Test
+    void addSpellTrapCard() {
+
+    }
+
+    @Test
+    void deleteAndDestroySpellTrap() {
+
+    }
+
+    @Test
+    void deleteAndDestroyAllSpellTrapCards() {
+
+    }
+
+    @Test
+    void isFull() {
+
+    }
+
+    @Test
+    void isThisCellOfPlayerSpellTrapFieldEmpty() {
+
+    }
+
+    @Test
+    void isThisCellOfOpponentSpellTrapFieldEmpty() {
+
+    }
+
+    @Test
+    void isFullPlace() {
+
+    }
+
+    @Test
+    void doesSpellTrapCardExistInField() {
+
+    }
+
+}
