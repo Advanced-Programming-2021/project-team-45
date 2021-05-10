8c3c9c6ec5e66d233bb6aa6d846aa18f1e1f8d6b
amir-haji <haji80as@gmail.com>
2021-05-10 23:17:37 +0430

    monster field test changed

--- src/test/java/model/game/fields/MonsterFieldTest.java
+++ src/test/java/model/game/fields/MonsterFieldTest.java
@@ -80,45 +80,60 @@ class MonsterFieldTest {
         assertTrue(monsterField.doesCardExist("Axe Raider"));
         monsterField.deleteAndDestroyMonster((MonsterCard) Card.getCardByName("Axe Raider"));
         assertFalse(monsterField.doesCardExist("Axe Raider"));
+        assertTrue(graveyard.doesCardExist("Axe Raider"));
     }
 
     @Test
     void getNumberOfMonstersInField() {
-
+        assertEquals(2, monsterField.getNumberOfMonstersInField());
+        monsterField.addMonsterToField((MonsterCard) Card.getCardByName("Battle OX"));
+        assertEquals(3, monsterField.getNumberOfMonstersInField());
     }
 
     @Test
     void isThisCellOfPlayerMonsterFieldEmpty() {
-
+        assertFalse(monsterField.isThisCellOfPlayerMonsterFieldEmpty(2));
+        assertTrue(monsterField.isThisCellOfPlayerMonsterFieldEmpty(7));
     }
 
     @Test
     void isThisCellOfOpponentMonsterFieldEmpty() {
-
+        assertFalse(monsterField.isThisCellOfOpponentMonsterFieldEmpty(2));
+        assertTrue(monsterField.isThisCellOfOpponentMonsterFieldEmpty(6));
     }
 
     @Test
     void isFull() {
-
+        assertFalse(monsterField.isFull());
+        monsterField.addMonsterToField((MonsterCard) Card.getCardByName("Wattkid"));
+        monsterField.addMonsterToField((MonsterCard) Card.getCardByName("Haniwa"));
+        assertTrue(monsterField.isFull());
     }
 
     @Test
     void isFullPlace() {
-
+        assertTrue(monsterField.isFull(4));
+        assertFalse(monsterField.isFull(6));
     }
 
     @Test
     void deleteMonster() {
-
+        assertTrue(monsterField.doesCardExist("Haniwa"));
+        monsterField.deleteMonster((MonsterCard) Card.getCardByName("Haniwa"));
+        assertFalse(monsterField.doesCardExist("Haniwa"));
+        assertTrue(graveyard.doesCardExist("Haniwa"));
     }
 
     @Test
     void deleteAndDestroyAllMonsters() {
-
+        assertEquals(4, monsterField.getNumberOfMonstersInField());
+        monsterField.deleteAndDestroyAllMonsters();
+        assertEquals(0, monsterField.getNumberOfMonstersInField());
     }
 
     @AfterAll
     public void setAfterTest() {
-
+        graveyard = null;
+        monsterField = null;
     }
 }
