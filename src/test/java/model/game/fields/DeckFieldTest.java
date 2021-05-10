e14aadae3f2078ded55a3993b3ece599810b1916
amir-haji <haji80as@gmail.com>
2021-05-10 21:00:32 +0430

    deck field test added

---
+++ src/test/java/model/game/fields/DeckFieldTest.java
@@ -0,0 +1,29 @@
+package model.game.fields;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DeckFieldTest {
+    @BeforeAll
+    public void setBeforeTest() {
+
+    }
+
+    @Test
+    void drawCard() {
+
+    }
+
+    @Test
+    void getFieldCard() {
+
+    }
+
+    @Test
+    void getDeck() {
+
+    }
+
+}
