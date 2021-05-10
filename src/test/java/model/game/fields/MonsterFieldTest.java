a5534b91bc75a0ee73ccebf390ff79b5b5ade131
amir-haji <haji80as@gmail.com>
2021-05-10 22:40:14 +0430

    monster field test added

---
+++ src/test/java/model/game/fields/MonsterFieldTest.java
@@ -0,0 +1,90 @@
+package model.game.fields;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MonsterFieldTest {
+
+    @BeforeAll
+    public void set() {
+
+    }
+
+    @Test
+    void getMonstersOnField() {
+
+    }
+
+    @Test
+    void getMonsterPositionArray() {
+
+    }
+
+    @Test
+    void getMonsterCardFromPlayerMonsterField() {
+
+    }
+
+    @Test
+    void getMonsterCardOpponentFromMonsterField() {
+
+    }
+
+    @Test
+    void getMonster() {
+
+    }
+
+    @Test
+    void deleteAttackedHistory() {
+
+    }
+
+    @Test
+    void deleteAndDestroyMonsters() {
+
+    }
+
+    @Test
+    void deleteAndDestroyAllMonsters() {
+
+    }
+
+    @Test
+    void deleteMonster() {
+
+    }
+
+    @Test
+    void getNumberOfMonstersInField() {
+
+    }
+
+    @Test
+    void isThisCellOfPlayerMonsterFieldEmpty() {
+
+    }
+
+    @Test
+    void isThisCellOfOpponentMonsterFieldEmpty() {
+
+    }
+
+    @Test
+    void isFull() {
+
+    }
+
+    @Test
+    void isFullPlace() {
+
+    }
+
+    @AfterAll
+    public void setAfterTest() {
+
+    }
+}
