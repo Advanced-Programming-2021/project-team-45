a8645cf9cabf651b206a8214a6a8067aaabfbc1d
Kasra <kasrahajian2002@gmail.com>
2021-05-11 01:44:06 +0430

    1st runable unfinished version

--- src/main/java/model/card/SpellTrapCards/AbilitiesOfTraps/CallOfTheHaunted.java
+++ src/main/java/model/card/SpellTrapCards/AbilitiesOfTraps/CallOfTheHaunted.java
@@ -1,16 +1,16 @@
-package model.card.SpellTrapCards.AbilitiesOfTraps;
-
-import model.game.Game;
-import model.card.Card;
-import model.card.MonsterCard;
-
-public class CallOfTheHaunted {
-    public static void ability(Game game){
-        // input one card from player graveyard
-        MonsterCard monsterCard;
-        Card card = game.getSelectedCard();
-        game.setSelectedCard(monsterCard);
-        game.summonMonster();
-        game.setSelectedCard(card);
-    }
-}
+//package model.card.SpellTrapCards.AbilitiesOfTraps;
+//
+//import model.game.Game;
+//import model.card.Card;
+//import model.card.MonsterCard;
+//
+//public class CallOfTheHaunted {
+//    public static void ability(Game game){
+//        // input one card from player graveyard
+//        MonsterCard monsterCard;
+//        Card card = game.getSelectedCard();
+//        game.setSelectedCard(monsterCard);
+//        game.summonMonster();
+//        game.setSelectedCard(card);
+//    }
+//}
