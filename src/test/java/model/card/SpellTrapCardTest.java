ce44e1bc921b6aa887d014b91fddcf1969621bbd
amir-haji <haji80as@gmail.com>
2021-05-12 22:11:38 +0430

    spell trap card test changed

--- src/test/java/model/card/SpellTrapCardTest.java
+++ src/test/java/model/card/SpellTrapCardTest.java
@@ -3,6 +3,8 @@ package model.card;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import java.util.Objects;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class SpellTrapCardTest {
@@ -14,6 +16,8 @@ class SpellTrapCardTest {
 
     @Test
     void isSpell() {
+        assertFalse(((SpellTrapCard) Objects.requireNonNull(Card.getCardByName("Time Seal"))).isSpell);
+        assertTrue(((SpellTrapCard) Card.getCardByName("Raigeki")).isSpell);
 
     }
 
@@ -24,17 +28,23 @@ class SpellTrapCardTest {
 
     @Test
     void getIcon() {
-
+        assertEquals(SpellAndTrapIcon.NORMAL, Card.getCardByName("Trap Hole"));
+        assertEquals(SpellAndTrapIcon.CONTINUOUS, Card.getCardByName("Supply Squad"));
+        assertEquals(SpellAndTrapIcon.COUNTER, Card.getCardByName("Negate Attack"));
     }
 
     @Test
     void summon() {
-
+        SpellTrapCard spellTrapCard = (SpellTrapCard) (Card.getCardByName("Magic Jammer")).clone();
+        spellTrapCard.summon();
+        assertEquals(SpellsAndTrapPosition.SUMMON ,spellTrapCard.position);
     }
 
     @Test
     void set() {
-
+        SpellTrapCard spellTrapCard = (SpellTrapCard) (Card.getCardByName("Terraforming").clone());
+        spellTrapCard.set();
+        assertEquals(SpellsAndTrapPosition.SET ,spellTrapCard.position);
     }
 
     @Test
