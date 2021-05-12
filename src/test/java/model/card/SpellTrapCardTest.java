2d206d4bd7e7b84a0142d4b6718076cf93e88fb1
amir-haji <haji80as@gmail.com>
2021-05-12 21:42:58 +0430

    spell trap card test added

---
+++ src/test/java/model/card/SpellTrapCardTest.java
@@ -0,0 +1,65 @@
+package model.card;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SpellTrapCardTest {
+
+    @BeforeAll
+    public static void beforeTest() {
+
+    }
+
+    @Test
+    void isSpell() {
+
+    }
+
+    @Test
+    void isActivated() {
+
+    }
+
+    @Test
+    void getIcon() {
+
+    }
+
+    @Test
+    void summon() {
+
+    }
+
+    @Test
+    void set() {
+
+    }
+
+    @Test
+    void getPosition() {
+
+    }
+
+    @Test
+    void setPosition() {
+
+    }
+
+    @Test
+    void setSpellOrTrapIcon() {
+
+    }
+
+    @Test
+    void setSpeedTrapAndSpell() {
+
+    }
+
+    @Test
+    void checkClone() {
+
+    }
+
+}
