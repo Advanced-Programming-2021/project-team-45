9c0b5126fd1719e971e7f5637874a26b5390db0b
amir-haji <haji80as@gmail.com>
2021-05-14 00:05:09 +0430

    LoginControllerTest added

---
+++ src/test/java/controller/LoginControllerTest.java
@@ -0,0 +1,29 @@
+package controller;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LoginControllerTest {
+    @BeforeAll
+    public static void setBeforeTest() {
+
+    }
+
+    @Test
+    void createUserErrorHandler() {
+
+    }
+
+    @Test
+    void loginUserErrorHandler() {
+
+    }
+
+    @AfterAll
+    public static void setAfterTest() {
+
+    }
+}
