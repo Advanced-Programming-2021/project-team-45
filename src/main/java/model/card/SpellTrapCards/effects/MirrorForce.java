82e6f3fc9e57d52921a19fa48c4b248eaee843b0
hm3623851 <79132538+hm3623851@users.noreply.github.com>
2021-05-18 10:29:45 +0430

    spell trap

--- src/main/java/model/card/SpellTrapCards/effects/MirrorForce.java
+++ src/main/java/model/card/SpellTrapCards/effects/MirrorForce.java
@@ -1,17 +1,22 @@
 package model.card.SpellTrapCards.effects;
 
 
+import model.card.SpellTrapCard;
 import model.card.SpellTrapCards.effects.RingOfDefense;
+import model.game.Chain;
 import model.game.Game;
 import model.game.GameBoard;
 import model.card.MonsterCard;
 import model.card.PositionMonsters;
 
 
-public class MirrorForce {
-    public static void ability(Game game){
-        if(canDoAbility(game)) {
-            GameBoard opponentGameBoard = game.getGameBoardOfOpponentPlayerOfThisTurn();
+public class MirrorForce extends Effect {
+
+
+    @Override
+    public void activate(Chain chain) {
+        if (canDoAbility(chain.getGame())) {
+            GameBoard opponentGameBoard = chain.getGame().getGameBoardOfOpponentPlayerOfThisTurn();
             for (MonsterCard monsterCard : opponentGameBoard.getMonsterField().getMonstersOnField()) {
                 if (monsterCard != null) {
                     if (monsterCard.getPosition() == PositionMonsters.ATTACK)
@@ -20,23 +25,30 @@ public class MirrorForce {
             }
         }
     }
-    private static boolean canDoAbility(Game game){
-        if(RingOfDefense.isThereRingOfDefenseInField(game)!=null){
-            if(RingOfDefense.ringOfDefenseHashMap.get(RingOfDefense.isThereRingOfDefenseInField(game)).getActive()){
+
+
+    private static boolean canDoAbility(Game game) {
+        if (RingOfDefense.isThereRingOfDefenseInField(game) != null) {
+            if (RingOfDefense.ringOfDefenseHashMap.get(RingOfDefense.isThereRingOfDefenseInField(game)).getActive()) {
+                game.getGameBoardOfPlayerOfThisTurn().getSpellTrapField().
+                        deleteAndDestroySpellTrap((SpellTrapCard) RingOfDefense.isThereRingOfDefenseInField(game));
                 return false;
-            }else{
+
+            } else {
                 return true;
             }
         }
         return true;
     }
 
-    public static boolean isThereAnyMirrorForce(GameBoard gameBoard){
-        for(int i=0;i<gameBoard.getSpellTrapField().getSpellTrapsArrayList().size();i++) {
+    public static boolean isThereAnyMirrorForce(GameBoard gameBoard) {
+        for (int i = 0; i < gameBoard.getSpellTrapField().getSpellTrapsArrayList().size(); i++) {
             if (gameBoard.getSpellTrapField().getSpellTrapsArrayList().get(i).getCardName().equals("Mirror Force")) {
                 return true;
             }
         }
         return false;
     }
+
+
 }
--- src/main/java/model/card/SpellTrapCards/effects/RingOfDefense.java
+++ src/main/java/model/card/SpellTrapCards/effects/RingOfDefense.java
@@ -2,24 +2,24 @@ package model.card.SpellTrapCards.effects;
 
 import model.card.Card;
 import model.card.SpellTrapCard;
+import model.game.Chain;
 import model.game.Game;
 import model.game.GameBoard;
 
 import java.util.HashMap;
 
-public class RingOfDefense {
+public class RingOfDefense extends Effect {
     private Boolean isActive;
-    public static HashMap<Card,RingOfDefense> ringOfDefenseHashMap;
+    public static HashMap<Card, RingOfDefense> ringOfDefenseHashMap;
 
-    RingOfDefense(SpellTrapCard card){
-        if(ringOfDefenseHashMap.get(card)==null){
-            ringOfDefenseHashMap.put(card,this);
-        }
-    }
 
-    public static void ability(SpellTrapCard card){
-        if(ringOfDefenseHashMap.get(card)!=null){
-            ringOfDefenseHashMap.get(card).isActive=true;
+    @Override
+    public void activate(Chain chain) {
+        Card card = isThereRingOfDefenseInField(chain.getGame());
+        if (card != null) {
+            if (ringOfDefenseHashMap.get(card) != null) {
+                ringOfDefenseHashMap.get(card).isActive = true;
+            }
         }
     }
 
@@ -28,17 +28,16 @@ public class RingOfDefense {
         return isActive;
     }
 
-    public static Card isThereRingOfDefenseInField(Game game){
-        GameBoard gameBoard=game.getGameBoardOfPlayerOfThisTurn();
-        for(int i=0;i<gameBoard.getSpellTrapField().getSpellTrapsArrayList().size();i++){
-            if(gameBoard.getSpellTrapField().getSpellTrapsArrayList().get(i).getCardName().equals("Ring Of Defense")){
+    public static Card isThereRingOfDefenseInField(Game game) {
+        GameBoard gameBoard = game.getGameBoardOfPlayerOfThisTurn();
+        for (int i = 0; i < gameBoard.getSpellTrapField().getSpellTrapsArrayList().size(); i++) {
+            if (gameBoard.getSpellTrapField().getSpellTrapsArrayList().get(i).getCardName().equals("Ring Of Defense")) {
+                ringOfDefenseHashMap.put(gameBoard.getSpellTrapField().getSpellTrapsArrayList().get(i)
+                        , new RingOfDefense());
                 return gameBoard.getSpellTrapField().getSpellTrapsArrayList().get(i);
             }
         }
         return null;
     }
-
-
-
 
 }
