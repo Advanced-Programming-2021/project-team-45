407bd91957c7be9f70345b549d90101d62665482
hm3623851 <79132538+hm3623851@users.noreply.github.com>
2021-06-12 18:23:26 +0430

    some bug fixed

--- src/main/java/view/gui/elements/GetImage.java
+++ src/main/java/view/gui/elements/GetImage.java
@@ -1,6 +1,7 @@
 package view.gui.elements;
 
 import javafx.scene.image.Image;
+import javafx.scene.shape.Rectangle;
 
 public class GetImage {
     public static Image getGameIcon() {
@@ -11,5 +12,9 @@ public class GetImage {
         String path = String.valueOf(GetImage.class.getResource("img"));
         path += "/" + imageName;
         return new Image(path);
+    }
+
+    public static Rectangle getCardsImages(String cardName){
+        return null;
     }
 }
