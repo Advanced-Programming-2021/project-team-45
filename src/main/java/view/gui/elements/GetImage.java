d81df88ba95816a5962e1dfba847af7a31dda024
hm3623851 <79132538+hm3623851@users.noreply.github.com>
2021-06-13 13:18:47 +0430

    some bug fixed

--- src/main/java/view/gui/elements/GetImage.java
+++ src/main/java/view/gui/elements/GetImage.java
@@ -1,7 +1,9 @@
 package view.gui.elements;
-
 import javafx.scene.image.Image;
+import javafx.scene.paint.ImagePattern;
 import javafx.scene.shape.Rectangle;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 
 public class GetImage {
     public static Image getGameIcon() {
@@ -15,6 +17,31 @@ public class GetImage {
     }
 
     public static Rectangle getCardsImages(String cardName){
-        return null;
+        Image image=findFile(cardName);
+        Rectangle rectangle=new Rectangle();
+        rectangle.setFill(new ImagePattern(image));
+        return rectangle;
+    }
+
+    private static Image findFile(String name){
+        name=name.trim();
+        String[] names=name.split(" ");
+        String finalName="";
+        for(int i=0;i<names.length;i++){
+            char u=names[i].charAt(0);
+            if(!(u>='A'&&u<='Z')){
+                u-=32;
+                String newName=u+names[i].substring(1);
+                names[i]=newName;
+            }
+            finalName+=names[i];
+        }
+        Image file= null;
+        try {
+            file = new Image(new FileInputStream("src/main/resources/view/gui/elements/cards/"+finalName+".jpg"));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        return file;
     }
 }
