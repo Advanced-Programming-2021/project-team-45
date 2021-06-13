8c77cbe424269fd6d1ae3e34f453c458156cbf53
Kasra <kasrahajian2002@gmail.com>
2021-06-14 02:36:45 +0430

    put cards on field correctly

--- src/main/java/view/gui/DuelMenuGui.java
+++ src/main/java/view/gui/DuelMenuGui.java
@@ -2,6 +2,7 @@ package view.gui;
 
 import javafx.scene.layout.*;
 import view.gui.elements.GameCard;
+import view.gui.elements.GameElementSize;
 import view.gui.elements.GetGameElements;
 import controller.GameController;
 import javafx.fxml.FXML;
@@ -49,18 +50,50 @@ public class DuelMenuGui extends MenuGui {
                 BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.DEFAULT,
                 BackgroundSize.DEFAULT);
         fieldPane.setBackground(new Background(backgroundImage));
+//        updateGameBoard();
 
-        fieldPane.getChildren().add(new GameCard(fieldPane, 110, 95));
+
+        ArrayList<String> opponentMonsterNames = new ArrayList<>();
+        opponentMonsterNames.add("Battle OX");
+        opponentMonsterNames.add("Yomi Ship");
+        opponentMonsterNames.add("back");
+        opponentMonsterNames.add("back");
+        opponentMonsterNames.add("back");
+        updateFields(opponentMonsterNames, "opponent_monster");
+        ArrayList<String> opponentSpellNames = new ArrayList<>();
+        opponentSpellNames.add("back");
+        opponentSpellNames.add("back");
+        opponentSpellNames.add("back");
+        opponentSpellNames.add("back");
+        opponentSpellNames.add("back");
+        updateFields(opponentSpellNames, "opponent_spell");
+        ArrayList<String> playerMonsterNames = new ArrayList<>();
+        playerMonsterNames.add("Suijin");
+        playerMonsterNames.add("Curtain of the dark ones");
+        playerMonsterNames.add("Feral Imp");
+        playerMonsterNames.add("Hero of the east");
+        playerMonsterNames.add("Battle warrior");
+        updateFields(playerMonsterNames, "player_monster");
+        updateFields(opponentSpellNames, "player_spell");
+
+
+
     }
 
     public void updateGameBoard() {
+        fieldPane.getChildren().removeAll();
         for (String field : cardFields) {
             updateFields(gameController.getFieldCards(field), field);
         }
     }
 
     public void updateFields(ArrayList<String> cardNames, String fieldName) {
-
+        int startX = GameElementSize.CARD_START_X.getSize();
+        int startY = GameElementSize.getYSizeByName(fieldName);
+        for (int i = 0; i < cardNames.size(); i++) {
+            fieldPane.getChildren().add(new GameCard(fieldPane, startX + (GameElementSize.CARD_DISTANCE.getSize() +
+                    GameElementSize.CARD_WIDTH.getSize()) * i, startY, cardNames.get(i)));
+        }
     }
 
 }
--- src/main/java/view/gui/elements/GameElementSize.java
+++ src/main/java/view/gui/elements/GameElementSize.java
@@ -1,21 +1,36 @@
 package view.gui.elements;
 
+
 public enum GameElementSize {
     CARD_WIDTH(51),
     CARD_HEIGHT(75),
-    CARD_DISTANCE(20),
-    CARD_START_X(105),
-    PLAYER_MONSTER_Y(320),
+    CARD_DISTANCE(26),
+    CARD_START_X(115),
+    PLAYER_MONSTER_Y(325),
     PLAYER_SPELL_Y(425),
-    OPPONENT_MONSTER_Y(205),
-    OPPONENT_SPELL_Y(110),
-    CARD_SELECTED_X(210),
-    CARD_SELECTED_Y(307);
+    OPPONENT_MONSTER_Y(200),
+    OPPONENT_SPELL_Y(100),
+    CARD_SELECTED_WIDTH(210),
+    CARD_SELECTED_HEIGHT(307);
 
     private final int size;
 
     GameElementSize(int size) {
         this.size = size;
+    }
+
+    public static int getYSizeByName(String name) {
+        switch (name) {
+            case "player_monster":
+                return PLAYER_MONSTER_Y.size;
+            case "player_spell":
+                return PLAYER_SPELL_Y.size;
+            case "opponent_monster":
+                return OPPONENT_MONSTER_Y.size;
+            case "opponent_spell":
+                return OPPONENT_SPELL_Y.getSize();
+        }
+        return 0;
     }
 
     public int getSize() {
