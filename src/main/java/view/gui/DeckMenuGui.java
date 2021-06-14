e5a0feb7e51e542c6578dc410894c758c7d1750e
hm3623851 <79132538+hm3623851@users.noreply.github.com>
2021-06-14 08:03:41 +0430

    Deck Menu Gui

---
+++ src/main/java/view/gui/DeckMenuGui.java
@@ -0,0 +1,11 @@
+package view.gui;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class DeckMenuGui extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+
+    }
+}
