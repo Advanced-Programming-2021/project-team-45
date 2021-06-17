a565052612621b9f6c4af1c287393f9c55b41c27
amir-haji <haji80as@gmail.com>
2021-06-17 20:04:00 +0430

    DeckFieldTest fixed

--- src/test/java/model/game/fields/DeckFieldTest.java
+++ src/test/java/model/game/fields/DeckFieldTest.java
@@ -1,8 +1,11 @@
 package model.game.fields;
 
+import model.Shop;
 import model.card.Card;
+import model.card.SpellAndTrapIcon;
 import model.card.SpellTrapCard;
 import model.user.User;
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
@@ -10,16 +13,26 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class DeckFieldTest {
 
-    private DeckField deckField;
+    private static DeckField deckField;
     @BeforeAll
-    public void setBeforeTest() {
+    public static void setBeforeTest() {
         User user = new User("hajji", "hajji", "hajji");
-        user.getCardInventory().addCardToInventory(Card.getCardByName("Yomi Ship"));
+        Shop shop = new Shop("hajji");
+
+        shop.buy("Yomi Ship");
+        shop.buy("Axe Raider");
+        shop.buy("Fireyarou");
+        shop.buy("Silver Fang");
+        shop.buy("Forest");
+        shop.buy("Closed Forest");
+
+        /*user.getCardInventory().addCardToInventory(Card.getCardByName("Yomi Ship"));
         user.getCardInventory().addCardToInventory(Card.getCardByName("Axe Raider"));
         user.getCardInventory().addCardToInventory(Card.getCardByName("Fireyarou"));
-        user.getCardInventory().addCardToInventory(Card.getCardByName("Siver Fang"));
+        user.getCardInventory().addCardToInventory(Card.getCardByName("Silver Fang"));
         user.getCardInventory().addCardToInventory(Card.getCardByName("Forest"));
-        user.getCardInventory().addCardToInventory(Card.getCardByName("Closed Forest"));
+        user.getCardInventory().addCardToInventory(Card.getCardByName("Closed Forest"));*/
+
         user.getUserDeck().createDeck("deck1", user);
         user.getUserDeck().getDeckByName("deck1").addCard("Yomi Ship",false, user);
         user.getUserDeck().getDeckByName("deck1").addCard("Axe Raider",false, user);
@@ -29,14 +42,14 @@ class DeckFieldTest {
         user.getUserDeck().getDeckByName("deck1").addCard("Closed Forest",false, user);
         user.getUserDeck().activateDeck("deck1");
 
-        this.deckField = new DeckField(user);
+        deckField = new DeckField(user);
 
     }
 
     @Test
     void drawCard() {
         User user = User.getUserByUsername("hajji");
-        Card card = this.deckField.drawCard();
+        Card card = deckField.drawCard();
         assertFalse(user.getUserDeck().getDeckByName("deck1").doesCardExist(card.getCardName(), false));
     }
 
@@ -45,13 +58,18 @@ class DeckFieldTest {
         User user = User.getUserByUsername("hajji");
         Card card = deckField.getFieldSpell();
         assertTrue(card instanceof SpellTrapCard);
-        assertTrue(((SpellTrapCard) card).getIcon().equals("Field"));
+        assertSame(((SpellTrapCard) card).getIcon(), SpellAndTrapIcon.FIELD);
     }
 
     @Test
     void getDeck() {
         User user = User.getUserByUsername("hajji");
         assertSame(user.getUserDeck().getDeckByName("deck1"), deckField.getDeck());
+    }
+
+    @AfterAll
+    public static void setAfterTest() {
+        User.deleteUserByUsername("hajji");
     }
 
 }
