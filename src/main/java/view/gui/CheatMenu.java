2c423b9748ca3283f23f2453c25e0649587c463d
Kasra <kasrahajian2002@gmail.com>
2021-07-09 01:27:53 +0430

    fixed cheat

--- src/main/java/view/gui/CheatMenu.java
+++ src/main/java/view/gui/CheatMenu.java
@@ -22,18 +22,14 @@ public class CheatMenu extends MenuGui {
 
     @Override
     public void start(Stage stage) throws Exception {
-        anchorPane= FXMLLoader.load(getClass().getResource("CheatMenu.fxml"));
-        Scene scene=new Scene(anchorPane);
-        Stage stage1=new Stage();
-        CheatMenu.stage=stage1;
+        anchorPane = FXMLLoader.load(getClass().getResource("CheatMenu.fxml"));
+        Scene scene = new Scene(anchorPane);
+        Stage stage1 = new Stage();
+        CheatMenu.stage = stage1;
         stage1.setOpacity(0.85);
         stage1.setTitle("cheat console");
         stage1.setScene(scene);
         stage1.show();
-    }
-
-    public static void main(String[] args) {
-        launch(args);
     }
 
     public void enterTheCheat(KeyEvent keyEvent) {
