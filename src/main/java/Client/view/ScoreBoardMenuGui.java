f9562e1c05445bfafc0a39e60f32cd14bddc0fee
amir-haji <haji80as@gmail.com>
2021-07-17 20:16:56 +0430

    scoreboard changed for server/client connection

--- src/main/java/Client/view/ScoreBoardMenuGui.java
+++ src/main/java/Client/view/ScoreBoardMenuGui.java
@@ -7,33 +7,67 @@ import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.effect.DropShadow;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.LinkedHashMap;
 
 public class ScoreBoardMenuGui extends MenuGui {
     private static Stage stage;
-    @FXML
-    public BorderPane borderpane;
-    @FXML
-    public GridPane scoreboard;
+
+    public static BorderPane borderpane;
+
+    public static GridPane scoreboard;
 
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("ScoreBoardMenu.fxml"));
         ScoreBoardMenuGui.stage = stage;
-        Scene scene = new Scene(root, 1080, 720);
+        borderpane = new BorderPane();
+
+        //top
+        Label label = new Label("YU-GI-OH!");
+        label.setFont(new Font("Arial Bold", 18));
+        label.setStyle("-fx-text-fill: white");
+        VBox vBox = new VBox(label);
+        vBox.setAlignment(Pos.CENTER);
+        vBox.setStyle("-fx-background-color: black");
+        vBox.setPrefSize(1080, 77);
+        borderpane.setTop(vBox);
+
+        //center
+        scoreboard = new GridPane();
+        scoreboard.setStyle("-fx-background-color: black");
+        borderpane.setCenter(scoreboard);
+
+        //bottom
+        Button backButton = new Button("back");
+        backButton.setPrefSize(100, 26);
+        backButton.setOnMouseClicked(e -> backToMainMenu());
+        backButton.setStyle("-fx-text-fill: white; -fx-background-color: red");
+        backButton.setEffect(new DropShadow());
+        VBox vBox1 = new VBox(backButton);
+        vBox1.setStyle("-fx-background-color: black");
+        vBox1.setAlignment(Pos.CENTER);
+        vBox1.setPrefSize(1080, 66);
+        borderpane.setBottom(vBox1);
+
+        Scene scene = new Scene(borderpane, 1080, 720);
         stage.setScene(scene);
         stage.setTitle("Score Board");
+        update();
     }
 
-    @FXML
-    void initialize() {
+
+
+    public static void update() {
         LinkedHashMap<String, Integer> list = ScoreboardController.getSortedNicknameScore();
         ArrayList<String> keyList = new ArrayList<>();
         keyList.addAll(list.keySet());
@@ -41,23 +75,29 @@ public class ScoreBoardMenuGui extends MenuGui {
         scoreboard.add(getLabel("rank", false), 0, 0);
         scoreboard.add(getLabel("nickname", false), 1, 0);
         scoreboard.add(getLabel("score", false), 2, 0);
-        for (int i = 0; i < list.size(); i++) {
+
+        setList(0,  0, keyList.get(0), list.get(keyList.get(0)));
+        int counter = 0;
+        for (int i = 1; i < list.size(); i++) {
             String key = keyList.get(i);
-            setList(i, key, list.get(key));
+            String beforeKey = keyList.get(i - 1);
+            if (list.get(key) != list.get(beforeKey))
+                counter++;
+            setList(i, counter, key, list.get(key));
         }
     }
 
-    private void setList(int i, String nickName, Integer score) {
+    private static void setList(int i, int rank, String nickName, Integer score) {
         boolean isLoginUser = false;
         if (nickName.equals(ScoreboardController.getNickname(username)))
             isLoginUser = true;
 
-        scoreboard.add(getLabel(String.valueOf(i + 1), isLoginUser), 0, i + 1);
+        scoreboard.add(getLabel(String.valueOf(rank + 1), isLoginUser), 0, i + 1);
         scoreboard.add(getLabel(nickName, isLoginUser), 1, i + 1);
         scoreboard.add(getLabel(String.valueOf(score), isLoginUser), 2, i + 1);
     }
 
-    private Label getLabel(String text, boolean isLoginUser) {
+    private static Label getLabel(String text, boolean isLoginUser) {
         Label label = new Label(text);
         label.setStyle("-fx-font-family: Arial;" +
                 "-fx-font-size: 13;" +
@@ -74,8 +114,11 @@ public class ScoreBoardMenuGui extends MenuGui {
         return label;
     }
 
-    public void backToMainMenu(MouseEvent mouseEvent) throws Exception {
+    public void backToMainMenu() {
         MainMenuGui mainMenuGui = new MainMenuGui();
-        mainMenuGui.start(stage);
+        try {
+            mainMenuGui.start(stage);
+        } catch(Exception e) {
+        }
     }
 }
