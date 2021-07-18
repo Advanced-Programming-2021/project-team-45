e4e218dffe0cde29d8ca4ac690648b2c3cf98c6e
hm3623851 <79132538+hm3623851@users.noreply.github.com>
2021-07-18 19:46:00 +0430

    deck finish

--- src/main/java/Server/ServerController/DeckRequestHandler.java
+++ src/main/java/Server/ServerController/DeckRequestHandler.java
@@ -1,19 +1,16 @@
 package Server.ServerController;
 
-import Client.view.DeckMenuGui;
 import Server.controller.DatabaseController;
 import Server.controller.DeckController;
-import Server.controller.DeckStarterController;
-import Server.model.card.Deck;
 import Server.model.user.User;
 
-import javax.crypto.spec.DESKeySpec;
 import java.net.Socket;
 
 public class DeckRequestHandler extends RequestHandler {
     public DeckRequestHandler(Socket socket) {
         super(socket);
     }
+
     private static DeckController deckController;
 
     public static void setDeckController(DeckController deckController) {
@@ -29,15 +26,43 @@ public class DeckRequestHandler extends RequestHandler {
         Object answer = "";
         User user = DatabaseController.getUserByToken(token);
         switch (methodName) {
+            case "initialize":
+                deckController.initialize();
+                break;
             case "setDeck":
                 deckController.setDeck(user.getUserDeck().getDeckByName((String) fields[0]));
                 break;
             case "deleteDeck":
-                user.getUserDeck().deleteDeckFromUserDecks(((Deck) fields[0]).getName());
+                deckController.deleteDeck();
                 break;
             case "setController":
                 setDeckController(new DeckController(DatabaseController.getUserByToken(token).getUsername()));
                 break;
+            case "deleteCardFromDeck":
+                deckController.deleteCardFromDeck((int) fields[0], (int) fields[1], (boolean) fields[2]);
+                break;
+            case "addCardToSideDeck":
+                deckController.addCardToSideDeck((String) fields[0]);
+                break;
+            case "addCardToMainDeck":
+                deckController.addCardToMainDeck((String) fields[0]);
+                break;
+            case "getAllCardNames":
+                answer=deckController.getAllCardsName();
+                break;
+            case "getMainCardNames":
+                answer=deckController.getMainCardNames();
+                break;
+            case "getSideCardNames":
+                answer=deckController.getSideCardNames();
+                break;
+            case "putCardToMainHashMap":
+                deckController.putCardToMainHashMap((int)fields[0],(int)fields[1],(String)fields[2]);
+                break;
+            case "putCardToSideHashMap":
+                deckController.putCardToSideHashMap((int)fields[0],(int)fields[1],(String)fields[2]);
+                break;
+
 
         }
         return fieldParser.getAnswer(answer);
